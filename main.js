cfdff90dfd05077ddb5888faceac89c5a198f8b6
Kbeach321 <iamkevinbeach@gmail.com>
2018-07-03 16:28:06 -0400

    CLEAN CODE

--- main.js
+++ main.js
@@ -1,59 +1,49 @@
-var now, hours, minutes, seconds, percent;
-var $clock = document.querySelector(".clock");
-console.log('$clock', $clock);
-
-// Displays Initial Time //
-var timeDisplay = hours + ":" + minutes + ":" + seconds
-console.log(timeDisplay);
-
+//--------------------  JS VARIABLES ----------------------//
+var now, hours, minutes, seconds, percent,
+ hoursHex, minutesHex, secondsHex, hexTime,
+ hoursPadded, minutesPadded, secondsPadded;
 var hover = false;
-
+//--------------------  HTML VARIABLES ----------------------//
+var $clock = document.querySelector(".clock");
 var $container = document.querySelector(".container");
+var $progress_bar = document.querySelector(".progress_bar");
+//--------------------  HTML FUNCTIONS ----------------------//
 $container.onmouseover = function(){
   hover = true;
 };
 $container.onmouseout = function(){
   hover = false;
 };
-
-
+//--------------------  JS FUNCTIONS ----------------------//
 setInterval(function(){
   now = new Date();
-
-  var hoursHex = ("0" + now.getHours().toString(16)).slice(-2);
-  var minutesHex = ("0" + now.getMinutes().toString(16)).slice(-2);
-  var secondsHex = ("0" + now.getSeconds().toString(16)).slice(-2);
-
-  var hexTime = hoursHex + ":" + minutesHex + ":" + secondsHex;
-
+//----------------- GENERATE HEX CODES ------------------//
+  hoursHex = ("0" + now.getHours().toString(16)).slice(-2);
+  minutesHex = ("0" + now.getMinutes().toString(16)).slice(-2);
+  secondsHex = ("0" + now.getSeconds().toString(16)).slice(-2);
+//------------------- HEX AS VALUE ------------------------//
+  hexTime = hoursHex + ":" + minutesHex + ":" + secondsHex;
+//------------------- HEX AS COLOR ------------------------//
   var hexbkg = "#" + hoursHex + minutesHex + secondsHex;
   $container.style.backgroundColor=hexbkg ;
-// Gets Hours --> Pads Hours //
+//------------------ GETS & PADS HOURS ------------------//
   hours = now.getHours();
-  var hoursPadded = hours.toString().padStart(2,'0');
-  // console.log('seconds padded', hoursPadded);
-
-// Gets Minutes --> Pads Hours //
+  hoursPadded = hours.toString().padStart(2,'0');
+//------------------ GETS & PADS MINS ------------------//
   minutes = now.getMinutes();
-  var minutesPadded = minutes.toString().padStart(2,'0');
-  // console.log('seconds padded', minutesPadded);
-
-// Gets Seconds --> Pads Hours //
+  minutesPadded = minutes.toString().padStart(2,'0');
+//------------------ GETS & PADS SECONDS ------------------//
   seconds = now.getSeconds();
-  var secondsPadded = seconds.toString().padStart(2,'0');
-  // console.log('seconds padded', secondsPadded);
-
-// Prints the padded content into the HTML //
+  secondsPadded = seconds.toString().padStart(2,'0');
+//------------------ DISPLAY ON .CLOCK ------------------//
 if(hover) {
   $clock.textContent = hexTime;
 } else {
   $clock.textContent = hoursPadded + ":" + minutesPadded + ":" + secondsPadded;
 }
-
-
-// Making a percent for the bar //
+//------------------ PERCENT FOR BAR ------------------//
   percent = (now.getSeconds() / 60) * 100;
   percent = Math.round(percent);
-  document.querySelector(".progress_bar").style.width=percent + "%";
+  $progress_bar.style.width = percent + "%";
 
 },1000);
